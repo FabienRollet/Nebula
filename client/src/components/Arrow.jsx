d4e0168167d07da08ed06aa32c5b012b946ef71e
Fabien Rollet <156512541+FabienRollet@users.noreply.github.com>
2024-10-21 11:54:23 +0200

    test image

--- client/src/components/Arrow.jsx
+++ client/src/components/Arrow.jsx
@@ -11,7 +11,7 @@ function Arrow() {
     >
       <img
         className="arrow"
-        src="client/src/assets/icons8-fusée-100.png"
+        src="/assets/icons8-fusée-100.png"
         alt="Arrow"
       />
       <p className="returnText">return top</p>
